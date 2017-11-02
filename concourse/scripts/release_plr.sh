edf007db8bd433b98cb10bfa569d749ed39be17a
Hubert Zhang <hubertzhang@apache.org>
2017-11-02 10:35:46 +0800

    Using git describe to get version.

--- concourse/scripts/release_plr.sh
+++ concourse/scripts/release_plr.sh
@@ -4,17 +4,19 @@ set -exo pipefail
 
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${CWDIR}/../../../
-
+pushd ${TOP_DIR}/plr_src
+PLR_VERSION=$(git describe | awk -F. '{printf("%d.%d.%d", $1, $2, $3)}')
+popd
 function release_gpdb5() {
     case "$OSVER" in
     suse11)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-sles11-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-sles11-x86_64.gppkg
       ;;
     centos6)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-rhel6-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-rhel6-x86_64.gppkg
       ;;
     centos7)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-rhel7-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-rhel7-x86_64.gppkg
       ;;
     *) echo "Unknown OS: $OSVER"; exit 1 ;;
   esac
@@ -23,13 +25,13 @@ function release_gpdb5() {
 function release_gpdb4() {
     case "$OSVER" in
     suse11)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-sles11-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-sles11-x86_64.gppkg
       ;;
     centos5)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-rhel5-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-rhel5-x86_64.gppkg
       ;;
     centos6)
-        cp bin_plr/plr-*.gppkg plr_gppkg/plr-2.3.1-$GPDBVER-rhel6-x86_64.gppkg
+        cp bin_plr/plr-*.gppkg plr_gppkg/plr-$PLR_VERSION-$GPDBVER-rhel6-x86_64.gppkg
       ;;
     *) echo "Unknown OS: $OSVER"; exit 1 ;;
     esac
